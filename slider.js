2bba16d0226f00ffc64bbd5646db570021f20eae
TuanNguyen-SA <fiestnema007@gmail.com>
2023-11-17 22:48:39 +0200

    Add images and some basic styling

--- slider.js
+++ slider.js
@@ -1,2 +1,7 @@
-// Test commit
-console.log("it works")
+const arrowLeft = document.querySelector(".arrowLeft");
+const arrowRight = document.querySelector(".arrowRight");
+const slider = document.querySelector(".slider");
+
+arrowRight.addEventListener("click",()=>{
+ slider.style.transform = `translateX(-500px)`
+})
