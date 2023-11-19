4d1d351dad81463dd5544ec55dbb04984098f71c
TuanNguyen-SA <fiestnema007@gmail.com>
2023-11-19 18:47:43 +0200

    minor fixes

---
+++ quiz.js
@@ -0,0 +1,12 @@
+const data = [
+	{
+	id: 1,
+	question: "Which day is the best day of the week?",
+	answer: [
+     {answer: "monday", isCorrect: false},
+	 {answer: "wednesday", isCorrect: false},
+	 {answer: "thursday", isCorrect: false},
+	 {answer: "saturday", isCorrect: true},
+	],
+     },]
+      
